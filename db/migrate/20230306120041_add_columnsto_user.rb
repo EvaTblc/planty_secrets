bda1ba6b59097908449fc2941330c62fdff24d51
Eva Touboulic <etouboulic@gmail.com>
2023-03-06 13:05:10 +0100

    setup devise complete

---
+++ db/migrate/20230306120041_add_columnsto_user.rb
@@ -0,0 +1,9 @@
+class AddColumnstoUser < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :username, :string
+    add_column :users, :latitude, :float
+    add_column :users, :longitude, :float
+  end
+end
