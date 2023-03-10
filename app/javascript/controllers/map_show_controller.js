72a621a162b3383a4edfe7e8f66367ffc90fa441
Eva Touboulic <etouboulic@gmail.com>
2023-03-10 14:05:22 +0100

    profile ENFIN fini pour cette semaine

--- app/javascript/controllers/map_show_controller.js
+++ app/javascript/controllers/map_show_controller.js
@@ -4,9 +4,6 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["plant", "map"]
 
-  connect() {
-    console.log(this.mapTarget);
-  }
   display() {
     console.log("Hello");
     this.mapTarget.classList.remove("d-none");
